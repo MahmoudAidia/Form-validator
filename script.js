e08c58bdfcc78589b999270e9a1c1d54e4529426
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2023-03-03 16:05:25 +0200

    Refactoring

--- script.js
+++ script.js
@@ -26,33 +26,55 @@ function isValidEmail(email) {
   return re.test(String(email).toLowerCase());
 }
 
+// Check Required Fields Function
+function checkRequired(inputArr) {
+  inputArr.forEach(function (element) {
+    if (element.value.trim() === "") {
+      showError(element, `${getFieldName(element)} is required`);
+    } else {
+      showSuccess(element);
+    }
+  });
+}
+
+// Making the first character of the Error message capital
+function getFieldName(name) {
+  return name.id.charAt(0).toUpperCase() + name.id.slice(1);
+}
+
+// Check the length of the username and the password
+function checkLength(value, min, max) {
+  if (value.value.length < min) {
+    showError(
+      value,
+      `${getFieldName(value)} must be more than ${min} Characters `
+    );
+  } else if (value.value.length > max) {
+    showError(
+      value,
+      `${getFieldName(value)}must be less than ${max} Characters `
+    );
+  } else {
+    showSuccess(value);
+  }
+}
+
+// Check Confirmation
+function checkConfirmation(item1, item2) {
+  if (item1.value === item2.value) {
+    showSuccess(item1);
+    showSuccess(itme2);
+  } else {
+    showError(item2, "The password is not matched");
+  }
+}
+
 // Event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === "") {
-    showError(email, "Email is required");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "Email is not valid");
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === "") {
-    showError(password, "Password is required");
-  } else {
-    showSuccess(password);
-  }
-
-  if (password2.value === "") {
-    showError(password2, "Confirmation is required");
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 20);
+  checkConfirmation(password, password2);
 });
